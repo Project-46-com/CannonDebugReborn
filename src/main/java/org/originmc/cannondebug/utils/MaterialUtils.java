ed4a57ee255d715380a694245b1f1c13ce5dfa1d
stan <dusek.s03@htlwienwest.at>
2022-12-15 17:46:59 +0100

    added other falling blocks except for sand

--- src/main/java/org/originmc/cannondebug/utils/MaterialUtils.java
+++ src/main/java/org/originmc/cannondebug/utils/MaterialUtils.java
@@ -25,9 +25,33 @@
 
 package org.originmc.cannondebug.utils;
 
+import com.google.common.collect.ImmutableList;
 import org.bukkit.Material;
 
+import java.util.List;
+
 public final class MaterialUtils {
+    private static final List<Material> fallingBlocks = ImmutableList.of(
+            Material.WHITE_CONCRETE_POWDER,
+            Material.ORANGE_CONCRETE_POWDER,
+            Material.MAGENTA_CONCRETE_POWDER,
+            Material.LIGHT_BLUE_CONCRETE_POWDER,
+            Material.YELLOW_CONCRETE_POWDER,
+            Material.LIME_CONCRETE_POWDER,
+            Material.PINK_CONCRETE_POWDER,
+            Material.GRAY_CONCRETE_POWDER,
+            Material.LIGHT_GRAY_CONCRETE_POWDER,
+            Material.CYAN_CONCRETE_POWDER,
+            Material.PURPLE_CONCRETE_POWDER,
+            Material.BLUE_CONCRETE_POWDER,
+            Material.BROWN_CONCRETE_POWDER,
+            Material.GREEN_CONCRETE_POWDER,
+            Material.RED_CONCRETE_POWDER,
+            Material.BLACK_CONCRETE_POWDER,
+            Material.SAND,
+            Material.RED_SAND,
+            Material.GRAVEL,
+            Material.ANVIL);
 
     /**
      * Identifies whether or not a material is used for dispensing tnt in tnt
@@ -59,7 +83,7 @@ public final class MaterialUtils {
      * @return true if material is used for stacking.
      */
     public static boolean isStacker(Material material) {
-        return material == Material.SAND || material == Material.GRAVEL;
+        return fallingBlocks.contains(material);
     }
 
     /**
